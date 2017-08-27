2f44b7e0a71cc811517dede46c13e4aa285c3f24
YCJ <cookie.jen422@gmail.com>
2017-08-27 14:52:33 -0700

    bulk upload in SPA  way

--- src/AppMVC/Areas/SPA/Controllers/SpaBulkUploadController.cs
+++ src/AppMVC/Areas/SPA/Controllers/SpaBulkUploadController.cs
@@ -50,17 +50,29 @@ namespace AppMVC.Areas.SPA.Controllers
         private List<Employee> GetEmployees(FileUploadViewModel model)
         {
             List<Employee> employees = new List<Employee>();
-            StreamReader csvreader = new StreamReader(model.fileUpload.InputStream);
-            csvreader.ReadLine();// Assuming first line is header
-            while (!csvreader.EndOfStream)
+
+            try
             {
-                var line = csvreader.ReadLine();
-                var values = line.Split(',');//Values are comma separated
-                Employee e = new Employee();
-                e.FirstName = values[0];
-                e.LastName = values[1];
-                e.Salary = int.Parse(values[2]);
-                employees.Add(e);
+                StreamReader csvreader = new StreamReader(model.fileUpload.InputStream);
+                csvreader.ReadLine();// Assuming first line is header
+                while (!csvreader.EndOfStream)
+                {
+                    var line = csvreader.ReadLine();
+                    var values = line.Split(',');//Values are comma separated
+                    Employee e = new Employee();
+                    e.FirstName = values[0];
+                    e.LastName = values[1];
+                    e.Salary = int.Parse(values[2]);
+                    employees.Add(e);
+                }
+            }
+            catch(FormatException fe)
+            {
+                throw new FormatException(fe.Message);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
             return employees;
         }

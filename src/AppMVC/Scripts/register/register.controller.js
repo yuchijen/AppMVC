5bd2f63903089398e14623c6c32cd4d337d9d4d8
ycj <yjen@syr.edu>
2017-12-06 16:35:00 -0800

    add dob and email fields in sign up page

--- src/AppMVC/Scripts/register/register.controller.js
+++ src/AppMVC/Scripts/register/register.controller.js
@@ -5,18 +5,38 @@
         .module('signin')
         .controller('RegisterController', RegisterController);
 
+    angular
+        .module('signin')
+        .directive('calendar', function () {
+            return {
+                require: 'ngModel',
+                link: function (scope, el, attr, ngModel) {
+                    $(el).datepicker({
+                        dateFormat: 'yy-mm-dd',
+                        onSelect: function (dateText) {
+                            scope.$apply(function () {
+                                ngModel.$setViewValue(dateText);
+                            });
+                        }
+                    });
+                }
+            };
+        });
+
+
     RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
     function RegisterController(UserService, $location, $rootScope, FlashService) {
         var vm = this;
 
         vm.register = register;
-
+        
         function register() {
             vm.dataLoading = true;
             UserService.Create(vm.user)
                 .then(function (response) {
                     if (response.success) {
                         FlashService.Success('Registration successful', true);
+                        debugger;
                         $location.path('/login');
                     } else {
                         FlashService.Error(response.message);
@@ -25,5 +45,23 @@
                 });
         }
     }
+
+
+
+    //function datePicker() {
+    //    vm.user.dob = new Date();
+
+    //    vm.minDate = new Date(
+    //        vm.user.dob.getFullYear(),
+    //        vm.user.dob.getMonth() - 2,
+    //        vm.user.dob.getDate()
+    //    );
+
+    //    vm.maxDate = new Date(
+    //        vm.user.dob.getFullYear(),
+    //        vm.user.dob.getMonth() + 2,
+    //        vm.user.dob.getDate()
+    //    );
+    //}
 
 })();

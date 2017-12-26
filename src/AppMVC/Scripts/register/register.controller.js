6c454671ce000c830e081fc5bea666ef763077b1
ycj <yjen@syr.edu>
2017-12-26 15:02:24 -0800

    add jQuery UI datePicker

--- src/AppMVC/Scripts/register/register.controller.js
+++ src/AppMVC/Scripts/register/register.controller.js
@@ -4,32 +4,16 @@
     angular
         .module('signin')
         .controller('RegisterController', RegisterController);
-
-    angular
-        .module('signin')
-        .directive('calendar', function () {
-            return {
-                require: 'ngModel',
-                link: function (scope, el, attr, ngModel) {
-                    $(el).datepicker({
-                        dateFormat: 'yy-mm-dd',
-                        onSelect: function (dateText) {
-                            scope.$apply(function () {
-                                ngModel.$setViewValue(dateText);
-                            });
-                        }
-                    });
-                }
-            };
-        });
-
-
+    
     RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
+
     function RegisterController(UserService, $location, $rootScope, FlashService) {
         var vm = this;
 
         vm.register = register;
-        
+        //vm.datePickerInti = datePickerInti;
+        datePickerInti();
+
         function register() {
             vm.dataLoading = true;
             UserService.Create(vm.user)
@@ -44,24 +28,22 @@
                     }
                 });
         }
+
+        function datePickerInti() {
+            $("#datepicker").datepicker({
+                beforeShow: function (input, inst) {
+                    setTimeout(function () {
+                        var rect = input.getBoundingClientRect();
+                        var topP = rect.top;
+                        var leftP = rect.right;
+                        inst.dpDiv.css({
+                            top: topP,
+                            left: leftP
+                        });
+                    }, 0);
+                }
+            });
+        }
     }
-
-
-
-    //function datePicker() {
-    //    vm.user.dob = new Date();
-
-    //    vm.minDate = new Date(
-    //        vm.user.dob.getFullYear(),
-    //        vm.user.dob.getMonth() - 2,
-    //        vm.user.dob.getDate()
-    //    );
-
-    //    vm.maxDate = new Date(
-    //        vm.user.dob.getFullYear(),
-    //        vm.user.dob.getMonth() + 2,
-    //        vm.user.dob.getDate()
-    //    );
-    //}
 
 })();
